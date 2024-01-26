bcf5334f66768d2329e416338854a057954ff003
ehsanjavadi1 <ehsanjavadip@gmail.com>
2024-01-26 15:06:56 +0330

    review3 Update Complete

--- Review/review3.dart
+++ Review/review3.dart
@@ -1,3 +1,36 @@
 void main() {
-  
+  //create array : LIST in dart
+  //1
+  var namesList = ['Ehsan', 'Erfan', 'Reza', 'Ali', 'Yeganeh'];
+  //print(namesList.runtimeType);
+  //print(namesList);
+
+  //2
+  List<String> familiesList = [
+    'Javadi',
+    'Javadi',
+    'Rahimi',
+    'Mir',
+    'Shahmorady'
+  ];
+  print(familiesList.runtimeType);
+  print(familiesList.length);
+  print(familiesList.reversed);
+  print(familiesList.first);
+  print(familiesList.last);
+  print("-------------------------------");
+  //familiesList.add("Dehbashi");
+  //familiesList.addAll(namesList);
+  //familiesList.clear();
+  String ele = "Mir";
+  familiesList[1] = "Noori";
+  //print(familiesList.indexOf(ele).toString());
+  familiesList.remove("Javadi");
+  print(familiesList);
+
+  //3
+  var shoppingList = List.filled(5, "Dart");
+  print(shoppingList);
+  //print(shoppingList.runtimeType);
+  //print(shoppingList);
 }
