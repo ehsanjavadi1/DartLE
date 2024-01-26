716568cedae2584d4a8de3517ede821274bba072
ehsanjavadi1 <ehsanjavadip@gmail.com>
2024-01-26 12:41:47 +0330

    review2 update 1

---
+++ Review/review2.dart
@@ -0,0 +1,21 @@
+void main() {
+  num number = 10.6;
+  // print(number);
+
+  // print(number.runtimeType);
+
+  //properties of integer
+  int num1 = 12;
+  // print(num1.runtimeType);
+
+  //int and double methods
+  double Moadel = 18.50;
+  num rmoadel = Moadel.round();
+  num absmoadel = Moadel.abs();
+  num ceilmoadel = Moadel.ceil();
+  num floormoadel = Moadel.floor();
+  String moadel = "Ehsan's Moadel is \$ " + ceilmoadel.toString();
+  String name = "Ehsan";
+  print(Moadel.toInt());
+  print(Moadel.runtimeType);
+}
